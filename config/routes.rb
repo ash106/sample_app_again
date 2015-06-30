b614099c250ada551cf28c4ebde0a8576e01c7b9
Alex Howington <alex.howington@gmail.com>
2015-06-29 19:31:04 -0500

    Finish login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get 'about', to: 'static_pages#about', as: :about
   get 'contact', to: 'static_pages#contact', as: :contact
   get 'signup', to: 'users#new', as: :signup
+  get 'login', to: 'sessions#new', as: :login
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy', as: :logout
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
